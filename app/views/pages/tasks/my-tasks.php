80668afaf6dbae52bbd7d63714c8ae5b6d66aafa
Cong <asdvbn666999@gmail.com>
2026-06-14 15:30:01 +0700

    chinh lai cau truc thu muc theo readme

--- app/views/pages/tasks/my-tasks.php
+++
@@ -1,8 +0,0 @@
-<div class="page-content">
-    <div class="page-header">
-        <h2>Task của tôi</h2>
-    </div>
-
-    <div class="app-card">
-        <p>Danh sách các task được gán cho bạn.</p>
-    </div>
